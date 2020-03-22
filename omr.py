5f06bc195a762ba3a11d26cd496444df9499787b
rbaron <rbaron@rbaron.net>
2020-03-22 10:42:43 +0100

    Makes use of Hu moments for scale and rotational invariant contour features

--- omr.py
+++ omr.py
@@ -4,23 +4,73 @@ import math
 import numpy as np
 
 
-CORNER_FEATS = (
-    0.322965313273202,
-    0.19188334690998524,
-    1.1514327482234812,
-    0.998754685666376,
-)
-
 TRANSF_SIZE = 512
+
+
+def calculate_contour_features(contour):
+    """Calculates interesting properties (features) of a contour.
+
+    We use these features to match shapes (contours). In this script,
+    we are interested in finding shapes in our input image that look like
+    a corner. We do that by calculating the features for many contours
+    in the input image and comparing these to the features of the corner
+    contour. By design, we know exactly what the features of the real corner
+    contour look like - check out the calculate_corner_features function.
+
+    It is crucial for these features to be invariant both to scale and rotation.
+    In other words, we know that a corner is a corner regardless of its size
+    or rotation. In the past, this script implemented its own features, but
+    OpenCV offers much more robust scale and rotational invariant features
+    out of the box - the Hu moments.
+    """
+    moments = cv2.moments(contour)
+    return cv2.HuMoments(moments)
+
+
+def calculate_corner_features():
+    """Calculates the array of features for the corner contour.
+    In practive, this can be pre-calculated, as the corners are constant
+    and independent from the inputs.
+
+    We load the img/corner.png file, which contains a single corner, so we
+    can reliably extract its features. We will use these features to look for
+    contours in our input image that look like a corner.
+    """
+    corner_img = cv2.imread('img/corner.png')
+    corner_img_gray = cv2.cvtColor(corner_img, cv2.COLOR_BGR2GRAY)
+    contours, hierarchy = cv2.findContours(
+        corner_img_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # We expect to see only two contours:
+    # - The "outer" contour, which wraps the whole image, at hierarchy level 0
+    # - The corner contour, which we are looking for, at hierarchy level 1
+    # If in trouble, one way to check what's happening is to draw the found contours
+    # with cv2.drawContours(corner_img, contours, -1, (255, 0, 0)) and try and find
+    # the correct corner contour by drawing one contour at a time. Ideally, this
+    # would not be done at runtime.
+    if len(contours) != 2:
+        raise RuntimeError(
+            'Did not find the expected contours when looking for the corner')
+
+    # Following our assumptions as stated above, we take the contour that has a parent
+    # contour (that is, it is _not_ the outer contour) to be the corner contour.
+    # If in trouble, verify that this contour is the corner contour with
+    # cv2.drawContours(corner_img, [corner_contour], -1, (255, 0, 0))
+    corner_contour = next(ct for i, ct in enumerate(contours)
+                             if hierarchy[0][i][3] != -1)
+
+    return calculate_contour_features(corner_contour)
 
 
 def normalize(im):
     return cv2.normalize(im, np.zeros(im.shape), 0, 255, norm_type=cv2.NORM_MINMAX)
 
+
 def get_approx_contour(contour, tol=.01):
     """Get rid of 'useless' points in the contour"""
     epsilon = tol * cv2.arcLength(contour, True)
     return cv2.approxPolyDP(contour, epsilon, True)
+
 
 def get_contours(image_gray):
     contours, hierarchy = cv2.findContours(
@@ -28,58 +78,34 @@ def get_contours(image_gray):
 
     return map(get_approx_contour, contours)
 
+
 def get_corners(contours):
+    CORNER_FEATS = calculate_corner_features()
     return sorted(
         contours,
-        key=lambda c: features_distance(CORNER_FEATS, get_features(c)))[:4]
+        key=lambda c: features_distance(CORNER_FEATS, calculate_contour_features(c)))[:4]
+
 
 def get_bounding_rect(contour):
     rect = cv2.minAreaRect(contour)
     box = cv2.boxPoints(rect)
     return np.int0(box)
 
-def get_convex_hull(contour):
-    return cv2.convexHull(contour)
-
-def get_contour_area_by_hull_area(contour):
-    return (cv2.contourArea(contour) /
-            cv2.contourArea(get_convex_hull(contour)))
-
-def get_contour_area_by_bounding_box_area(contour):
-    return (cv2.contourArea(contour) /
-            cv2.contourArea(get_bounding_rect(contour)))
-
-def get_contour_perim_by_hull_perim(contour):
-    return (cv2.arcLength(contour, True) /
-            cv2.arcLength(get_convex_hull(contour), True))
-
-def get_contour_perim_by_bounding_box_perim(contour):
-    return (cv2.arcLength(contour, True) /
-            cv2.arcLength(get_bounding_rect(contour), True))
-
-def get_features(contour):
-    try:
-        return (
-            get_contour_area_by_hull_area(contour),
-            get_contour_area_by_bounding_box_area(contour),
-            get_contour_perim_by_hull_perim(contour),
-            get_contour_perim_by_bounding_box_perim(contour),
-        )
-    except ZeroDivisionError:
-        return 4*[np.inf]
 
 def features_distance(f1, f2):
     return np.linalg.norm(np.array(f1) - np.array(f2))
 
-# Default mutable arguments should be harmless here
+
 def draw_point(point, img, radius=5, color=(0, 0, 255)):
     cv2.circle(img, tuple(point), radius, color, -1)
+
 
 def get_centroid(contour):
     m = cv2.moments(contour)
     x = int(m["m10"] / m["m00"])
     y = int(m["m01"] / m["m00"])
     return (x, y)
+
 
 def order_points(points):
     """Order points counter-clockwise-ly."""
@@ -92,13 +118,14 @@ def order_points(points):
 
     return sorted(points, key=positive_angle)
 
+
 def get_outmost_points(contours):
     all_points = np.concatenate(contours)
     return get_bounding_rect(all_points)
 
+
 def perspective_transform(img, points):
     """Transform img so that points are the new corners"""
-
     source = np.array(
         points,
         dtype="float32")
@@ -115,11 +142,13 @@ def perspective_transform(img, points):
     warped = cv2.warpPerspective(img, transf, (TRANSF_SIZE, TRANSF_SIZE))
     return warped
 
+
 def sheet_coord_to_transf_coord(x, y):
     return list(map(lambda n: int(np.round(n)), (
         TRANSF_SIZE * x/744.055,
         TRANSF_SIZE * (1 - y/1052.362)
     )))
+
 
 def get_question_patch(transf, q_number):
     # Top left
@@ -135,9 +164,11 @@ def get_question_patch(transf, q_number):
     )
     return transf[tl[1]:br[1], tl[0]:br[0]]
 
+
 def get_question_patches(transf):
     for i in range(1, 11):
         yield get_question_patch(transf, i)
+
 
 def get_alternative_patches(question_patch):
     for i in range(5):
@@ -145,11 +176,13 @@ def get_alternative_patches(question_patch):
         x1, _ = sheet_coord_to_transf_coord(50 + 100 * i, 0)
         yield question_patch[:, x0:x1]
 
+
 def draw_marked_alternative(question_patch, index):
     cx, cy = sheet_coord_to_transf_coord(
         50 * (2 * index + .5),
         50/2)
     draw_point((cx, TRANSF_SIZE - cy), question_patch, radius=5, color=(255, 0, 0))
+
 
 def get_marked_alternative(alternative_patches):
     means = list(map(np.mean, alternative_patches))
@@ -161,8 +194,10 @@ def get_marked_alternative(alternative_patches):
 
     return np.argmin(means)
 
+
 def get_letter(alt_index):
     return ["A", "B", "C", "D", "E"][alt_index] if alt_index is not None else "N/A"
+
 
 def get_answers(source_file):
     """Run the full pipeline:
@@ -178,6 +213,8 @@ def get_answers(source_file):
         - Scan each line for the marked answer
     """
 
+    corner_features = calculate_corner_features()
+
     im_orig = cv2.imread(source_file)
 
     blurred = cv2.GaussianBlur(im_orig, (11, 11), 10)
@@ -187,6 +224,7 @@ def get_answers(source_file):
     ret, im = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY)
 
     contours = get_contours(im)
+
     corners = get_corners(contours)
 
     cv2.drawContours(im_orig, corners, -1, (0, 255, 0), 3)
@@ -204,11 +242,8 @@ def get_answers(source_file):
 
         answers.append(get_letter(alt_index))
 
-    #cv2.imshow('orig', im_orig)
-    #cv2.imshow('blurred', blurred)
-    #cv2.imshow('bw', im)
-
     return answers, transf
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -238,14 +273,12 @@ def main():
 
     if args.output:
         cv2.imwrite(args.output, im)
-        print("Wrote image to {}".format(args.output))
+        print('Wrote image to {}.'.format(args.output))
 
     if args.show:
-        cv2.imshow('trans', im)
+        cv2.imshow('Annotated image', im)
+        cv2.waitKey(0)
 
-        print("Close image window and hit ^C to quit.")
-        while True:
-            cv2.waitKey()
 
 if __name__ == '__main__':
     main()
